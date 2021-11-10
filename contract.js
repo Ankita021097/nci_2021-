c0753a9fc27b0302be6f5b994e7eea33fa7f5cde
ankita <you@example.com>
2021-11-10 14:26:28 +0000

    added web3

--- contract.js
+++ contract.js
@@ -1,3 +1,9 @@
 // file to read the contract interactions
 
+const web3= require("Web3")
+
+//instantiate web3
+const web3 = new web3(rpcURL);
+console.log ("connected to web3");
+
 console.log("hello world?");
